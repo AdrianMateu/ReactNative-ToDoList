cb6009c2c011384c352f6529e09071b5fd582373
Adrián Mateu Martínez <adrianmateumartinez@MacBook-Pro-de-Adrian.local>
2020-03-07 12:53:13 +0100

    Creation styles

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,33 @@
-import React, { Fragment } from 'react';
-import { Text, View } from 'react-native';
+import React, { Fragment } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	listItem: {
+		borderWidth: 1,
+		margin: 5,
+		width: "80%"
+	},
+	text: {
+		padding: 5,
+		fontWeight: "bold"
+	},
+	textDone: {
+		color: "#aaa",
+		textDecorationLine: "line-through",
+		fontWeight: "normal"
+	}
+});
 
 const TodoList = ({ todos }) => (
-    <Fragment>
-        { todos.map( todo => ( !todo.done && <Text key={todo.text}>{todo.text}</Text>))}
-    </Fragment>
+	<Fragment>
+		{todos.map(todo => (
+			<View style={styles.listItem} key={todo.text}>
+				<Text style={[styles.text, todo.done && styles.textDone]}>
+					{todo.text}
+				</Text>
+			</View>
+		))}
+	</Fragment>
 );
 
 export default TodoList;
--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,38 +1,44 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import TodoList from "todoList/src/components/TodoList/index";
-import {getTodos} from "todoList/src/data/todos";
+import { getTodos } from "todoList/src/data/todos";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		margin: 4
+	}
+});
 
-class MainScreen extends Component{
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-        todos: []
-      };
-    };
-    
-    componentDidMount = () => {
-        this.setState({ todos: getTodos() });
-    }
+class MainScreen extends Component {
+	constructor(props) {
+		super(props);
 
-    render (){
+		this.state = {
+			todos: []
+		};
+	}
 
-        return(
-            <View style= {styles.container}>
-                <Text selectable>ToDo List App</Text>
-                <TodoList todos={this.state.todos}/>
-            </View>
-        );
-    } 
+	componentDidMount = () => {
+		this.setState({ todos: getTodos() });
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.title} selectable>
+					ToDo List App
+				</Text>
+				<TodoList todos={this.state.todos} />
+			</View>
+		);
+	}
 }
 export default MainScreen;
