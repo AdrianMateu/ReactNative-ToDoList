4315888aeb6a1b527edf00d36b8f6863ddf5984c
Adrián Mateu Martínez <adrianmateumartinez@MacBook-Pro-de-Adrian.local>
2020-03-29 13:00:52 +0200

    Change status bar light-content

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { StatusBar } from "react-native";
 
 export default class App extends React.Component {
 	render() {
-		return <AppNavigator />;
+		return (
+			<React.Fragment>
+				<StatusBar barStyle="light-content" />
+				<AppNavigator />
+			</React.Fragment>
+		);
 	}
 }
